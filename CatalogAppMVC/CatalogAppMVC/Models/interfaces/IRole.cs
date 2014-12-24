5409361e13c484a5be447cfe303391eaa5d0be7a
OlegSkomorohov <skom_oleg@hotmail.com>
2014-12-24 10:49:26 +0300

    Имплиментация IRole

---
+++ CatalogAppMVC/CatalogAppMVC/Models/interfaces/IRole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogAppMVC.Models.interfaces
+{
+    interface IRole
+    {
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long MaxSizeDownloadFileInternal { get; set; }
+
+        public long MaxSizeDownloadFileExternal { get; set; }
+
+        public long MaxSizeDownloadOnDay { get; set; }
+
+        public bool CanDownloadFile { get; set; }
+
+        //TODO: interface IRole Проверить абстракцию типа коллекции.
+        public ICollection<CatalogAppMVC.Models.LinqToSqlMdl.CatalogCategories> AccessibleCategories();
+
+        //TODO: interface IRole Установка бонуса в МБ за спасибо.
+
+    }
+}
