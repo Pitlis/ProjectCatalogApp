f04132f43bdf6ad69d0c85566c41fb4d37f20b15
pitlis <pitlis95@gmail.com>
2014-12-26 17:50:43 +0300

    Интерфейсы для парсеров.

---
+++ CatalogAppMVC/CatalogAppMVC/Models/interfaces/IParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parser_infofrezer_ru
+{
+    public interface IParser
+    {
+        Dictionary<string, string> GetCategories(string categoriesURL);
+        Dictionary<string, string> GetRecordsFromCategory(string categoryURL);
+        IRecord GetRecord(string recordURL);
+    }
+}
